ff5009b8ff0dc9c494b565e00b020b12d4028a47
Sonia Binty <sonianazninbinty@gmail.com>
2024-12-24 03:02:16 +0600

    added service by id

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require("cors")
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 app.use(cors())
 app.use(express.json())
 
@@ -28,6 +28,8 @@ async function run() {
 
  const serviceCollection = client.db("serviceDB").collection('service')
 
+
+// service post
  app.post('/service', async(req, res) =>{
   const newService = req.body
   console.log(newService)
@@ -35,12 +37,24 @@ async function run() {
   res.send(result)
  })
    
-
+// service get
  app.get('/service', async(req, res) =>{
   const cursor=serviceCollection.find()
   const result = await cursor.toArray()
   res.send(result)
  })
+
+
+//  detail by id
+
+
+app.get("/service/:id", async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id) };
+  const result = await serviceCollection.findOne(query);
+  res.send(result);
+  
+    });
 
 
 
